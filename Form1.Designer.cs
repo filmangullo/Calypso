b2eaf8f646395a54f90e3eaab524c10a59a590e3
Filimantaptius Gulo <filmantapgullo@gmail.com>
2016-06-25 09:31:46 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace QA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(96, 179);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 0;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(111, 205);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "Check";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Location = new System.Drawing.Point(12, 20);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(260, 141);
+            this.lblQuestion.TabIndex = 2;
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtAnswer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QA
+{
+    public partial class Form1 : Form
+    {
+        private int index = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private String[] questions = {"Apa faktor yang menyebabkan seseorang ingin menikah?","Pertanyaan 2","Pertanyaan 3" };
+        private String[,] answers = { { "Umur", "Anak-Anak", "Remaja", "Dewasa" }, { "2a", "2b", "2c", "2d" }, { "3a", "3b", "3c", "3d" } };
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (indexOfMe(answers, txtAnswer.Text) > -1)
+            {
+                this.Text =  answers[index, 0];
+            }
+            else
+            {
+                this.Text = "Salah";
+            }
+        }
+         
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblQuestion.Text = questions[index]; 
+        }
+        private int indexOfMe(String[,] obj,String kata)
+        {
+            for (int i = 0; i < obj.GetUpperBound(1)+1; i++)
+            {
+                if (obj[index, i].Equals(kata))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            index++;
+            lblQuestion.Text = questions[index];
+        }
+    }
+}
